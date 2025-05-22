c23e3909415f4b7b6c90000fa85b59148955f65a
juanfuc <juanfuc@gmail.com>
2025-05-22 08:07:05 -0500

    adding 1.png

---
+++ docs/_app/immutable/chunks/paths.CkDVCoZa.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_dbq71t)==null?void 0:s.base)??"/montajes_aep_2025-1";var t;const e=((t=globalThis.__sveltekit_dbq71t)==null?void 0:t.assets)??a;export{e as a,a as b};
