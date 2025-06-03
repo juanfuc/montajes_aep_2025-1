6d1b1023fd0b62537f64e0580774401c32108af7
juanfuc <juanfuc@gmail.com>
2025-06-03 14:24:40 -0500

    Con montajes 9 y 12

---
+++ docs/_app/immutable/chunks/paths.DcF0HuST.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1qi9j7f)==null?void 0:s.base)??"/montajes_aep_2025-1";var a;const t=((a=globalThis.__sveltekit_1qi9j7f)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ docs/_app/immutable/entry/start.DDSpK_UX.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D7gX1tQF.js";export{t as start};
